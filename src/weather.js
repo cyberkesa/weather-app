0c9fefe17aaeb732e7cbdd5b00dab010bc36bfc6
cyberkesa <nastya63rus@mail.ru>
2023-06-12 05:56:21 +0300

    Move input field to the bottom of the screen

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './Weather.css'; 
+import "./Weather.css";
 
 const Weather = () => {
   const [city, setCity] = useState("");
@@ -26,13 +26,41 @@ const Weather = () => {
 
   return (
     <div className="container">
-      <h2
-        className="text-center"
-        style={{ marginTop: "30px", marginBottom: "30px" }}
-      >
-        Weather App
-      </h2>
-      <form onSubmit={handleSubmit} className="my-4">
+      <h2 className="text-center">Weather App</h2>
+      {weatherData && (
+        <div className="card">
+          <div className="card-body">
+            <h3 className="location">{weatherData.name}, {weatherData.sys.country}</h3>
+            <p className="temperature">{weatherData.main.temp}°C</p>
+            <p className="card-text" style={{ display: "inline-block" }}>
+              Погода: {weatherData.weather[0].description}
+            </p>
+            <img
+			  className="weather-icon"
+              src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+              style={{ display: "inline-block", marginLeft: "10px" }}
+              alt="Weather Icon"
+            />
+            <p className="card-text">Влажность: {weatherData.main.humidity}%</p>
+            <p className="card-text">
+              Скорость ветра: {weatherData.wind.speed} м/с
+            </p>
+            <p className="card-text">Облачность: {weatherData.clouds.all}%</p>
+            <p className="card-text">
+              Давление: {weatherData.main.pressure} гПа
+            </p>
+            <p className="card-text">
+              Восход солнца:{" "}
+              {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}
+            </p>
+            <p className="card-text">
+              Заход солнца:{" "}
+              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
+            </p>
+          </div>
+        </div>
+      )}
+      <form onSubmit={handleSubmit} className="input-form">
         <div className="input-group">
           <input
             type="text"
@@ -60,38 +88,6 @@ const Weather = () => {
           </div>
         </div>
       </form>
-      {weatherData && (
-        <div className="card">
-          <div className="card-body">
-            <h3 className="location">{weatherData.name}, {weatherData.sys.country}</h3>
-            <p className="temperature">{weatherData.main.temp}°C</p>
-            <p className="card-text" style={{ display: "inline-block" }}>
-              Погода: {weatherData.weather[0].description}
-            </p>
-            <img
-              src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
-              style={{ display: "inline-block", marginLeft: "10px" }}
-              alt="Weather Icon"
-            />
-            <p className="card-text">Влажность: {weatherData.main.humidity}%</p>
-            <p className="card-text">
-              Скорость ветра: {weatherData.wind.speed} м/с
-            </p>
-            <p className="card-text">Облачность: {weatherData.clouds.all}%</p>
-            <p className="card-text">
-              Давление: {weatherData.main.pressure} гПа
-            </p>
-            <p className="card-text">
-              Восход солнца:{" "}
-              {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}
-            </p>
-            <p className="card-text">
-              Заход солнца:{" "}
-              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
-            </p>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
